235d65808c9e6bbd45447c831c768e5aca6d1d2a
Agam Yadav <agam@Mac9411.local>
2025-04-03 12:14:48 +0530

    updated frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,10 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173" || process.env.CORS_ORIGIN,
+    origin: ["https://inventory-management-xi-rouge.vercel.app", "http://localhost:5173"], 
     credentials: true
-}))
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
